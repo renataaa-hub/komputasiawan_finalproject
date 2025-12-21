1719e77c5ef90dbccd5540a35f258047d102ccb5
thaffrl <althaffarrelbusiness@gmail.com>
2025-12-21 16:00:18 +0700

    Beda Akses: Basic, Pro, Premium update

--- resources/views/subscription-payment.blade.php
+++ resources/views/subscription-payment.blade.php
@@ -82,7 +82,7 @@
     @if(config('midtrans.is_production'))
     <script src="https://app.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
     @else
-    <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
+    <script src="https://app.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
     @endif
     
     <script type="text/javascript">
